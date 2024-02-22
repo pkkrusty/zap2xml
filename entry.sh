67e5ded7ceba64daf69854389a42d195c312929b
Zach Butler <kj4ezj@pm.me>
2024-02-22 16:52:59 -0500

    Shellcheck SC2006: Use $(...) notation instead of legacy backticked `...`

--- entry.sh
+++ entry.sh
@@ -4,7 +4,7 @@
 #/usr/sbin/crond -f -l 8
 
 while true; do
-    DATE=`date`
+    DATE="$(date)"
     /zap2xml.pl -u $USERNAME -p $PASSWORD -U -o /data/$XMLTV_FILENAME $OPT_ARGS
     echo "Last run time: $DATE"
     echo "Will run in $SLEEPTIME seconds"
