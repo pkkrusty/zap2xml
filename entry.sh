10bf7331bd215ea8de86ef8e02618a45bf041778
Zach Butler <kj4ezj@pm.me>
2024-02-22 16:51:00 -0500

    bashate E002: Tab indents

--- entry.sh
+++ entry.sh
@@ -5,9 +5,9 @@
 
 while :
 do
-	DATE=`date`
-	/zap2xml.pl -u $USERNAME -p $PASSWORD -U -o /data/$XMLTV_FILENAME $OPT_ARGS
-	echo "Last run time: $DATE"
-	echo "Will run in $SLEEPTIME seconds"
-	sleep $SLEEPTIME
+    DATE=`date`
+    /zap2xml.pl -u $USERNAME -p $PASSWORD -U -o /data/$XMLTV_FILENAME $OPT_ARGS
+    echo "Last run time: $DATE"
+    echo "Will run in $SLEEPTIME seconds"
+    sleep $SLEEPTIME
 done
