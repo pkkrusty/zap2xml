8a3635bf4170a474fca1b899de3e11651175363c
Zach Butler <kj4ezj@pm.me>
2024-02-22 16:43:29 -0500

    Lint zap2xml whitespace for consistency

--- .github/workflows/lint.sh
+++ .github/workflows/lint.sh
@@ -3,6 +3,8 @@ set -eo pipefail
 echo "Begin - ${0##*/}"
 
 # lint CI code
+printf '\e[1;35m===== Lint zap2xml.pl Whitespace =====\e[0m\n'
+ee bashate -i E006,E010,E040 zap2xml.pl
 printf '\e[37m===== Lint CI =====\e[0m\n'
 ee bashate -i E006 .github/workflows/deps.sh
 ee shellcheck -x -f gcc .github/workflows/deps.sh
